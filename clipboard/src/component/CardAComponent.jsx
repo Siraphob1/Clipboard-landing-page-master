877b6624461d5dc441cbb71bc5037ac219381ea7
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-09 16:46:40 +0700

    ch10 create propsname in CardAComponent

--- clipboard/src/component/CardAComponent.jsx
+++ clipboard/src/component/CardAComponent.jsx
@@ -3,14 +3,13 @@ import PropTypes from 'prop-types';
 
 const CardAComponent = (props) => {
 
-    const {title , paragraph , titles,paragraphs } = props
+    const {title , paragraph , classtitle,classparagraph } = props
 
     
   return (
     <div className='text-center border'>
-        <h1 className={` text-[#4c545d] ${titles} font-semibold mb-4 `}>{title}</h1>
-        <p className={` text-[#9fabb2] ${paragraphs} px-[1rem] border
-                        lg:px-[3.4rem]`}>{paragraph}</p>
+        <h1 className={` text-[#4c545d] ${classtitle} font-semibold mb-4 `}>{title}</h1>
+        <p className={` text-[#9fabb2] ${classparagraph}  border`}>{paragraph}</p>
     </div>
   )
 }
@@ -22,7 +21,7 @@ export default CardAComponent
 CardAComponent.propTypes ={
     title:PropTypes.string,
     paragraph:PropTypes.string,
-    titles:PropTypes.string,
-    paragraphs:PropTypes.string
+    classtitle:PropTypes.string,
+    classparagraph:PropTypes.string
    
 };
