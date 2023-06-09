0bb3b42fea3fb07daeee316912b533c9fb70fb3d
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-09 18:37:20 +0700

    ch10 fix class for title& paragraph create btn

--- clipboard/src/component/MainComponent.jsx
+++ clipboard/src/component/MainComponent.jsx
@@ -15,19 +15,19 @@ const MainComponent = () => {
             title:"A history of everything you copy",
             paragraph:"Clipboard allows you to track and organize everything you        copy. Instantly access your clipboard on all your devices.",
             classtitle: "text-[2rem]",
-            classparagraph:"text-[1rem] px-[1rem]"
+            classparagraph:"text-[1rem] px-[1rem] lg:px-[28rem]"
         },
         {
             title:"Keep track of your snippets",
             paragraph:"Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything.",
             classtitle: "text-[1.8rem]",
-            classparagraph:"text-[1rem] px-[1rem]"
+            classparagraph:"text-[1rem] px-[1rem] lg:px-[25rem]"
         },
         {
             title:"Access Clipboard anywhere",
             paragraph:"Whether you’re on the go, or at your computer, you can access all your Clipboard snippets in a few simple clicks.",
-            classtitle: "text-[1.8rem]",
-            classparagraph:"text-[1rem] px-[1rem]"
+            classtitle: "text-[1.8rem] capitalize",
+            classparagraph:"text-[1rem] px-[1rem] lg:px-[25rem]"
         },
         {
             title:"Supercharge your workflow",
@@ -38,14 +38,14 @@ const MainComponent = () => {
         {
             title:"Clipboard for iOS and Mac OS",
             paragraph:"Available for free on the App Store. Download for Mac or iOS, sync with iCloud and you’re ready to start adding to your clipboard.",
-            classtitle: "text-[1.8rem]",
-            classparagraph:"text-[1rem] px-[1rem]"
+            classtitle: "text-[2rem]",
+            classparagraph:"text-[1rem] px-[1.1rem] lg:px-[26rem]"
         }    
     ]
 
   return (
     // lg:w-6/12
-    <main className=" mx-auto w-11/12 lg:w-full">  
+    <main className=" mx-auto w-11/12 bg-white lg:w-full">  
         <CardAComponent {...data[0]}/>
         <div className="flex flex-col mx-auto mt-12 mb-[8rem] w-11/12 
                          lg:flex-row  lg:justify-center  lg:mx-0  lg:w-auto ">
@@ -71,15 +71,20 @@ const MainComponent = () => {
             <CardCComponent/>
         </div>
         <div className="flex flex-col items-center mb-[6rem]
-                        lg:flex-row lg:items-start  lg:justify-between lg:px-[10rem] lg:mb-[8rem]">           
+                        lg:flex-row lg:items-start  lg:justify-between lg:px-[10rem] lg:mb-[9rem]">           
             <img src={igoogle} alt="igoogle" className="mb-[4rem] lg:mb-0" />
             <img src={iibm} alt="iibm" className="mb-[4rem] lg:mb-0" />
             <img src={imicrosoft} alt="imicrosoft" className="mb-[4rem] lg:mb-0" />
             <img src={ihp} alt="ihp" className="mb-[4rem] lg:mb-0" />
             <img src={ivector} alt="ivector" className="mb-[4rem] lg:mb-0" />
         </div>
-        <div className="mb-[4rem] lg:mb-[8rem]">
+        <div className="mb-[4rem] lg:mb-[1rem]">
             <CardAComponent {...data[4]}/>
+        </div>
+        <div className="flex flex-col mx-auto mt-12 mb-[8rem] w-11/12 
+                         lg:flex-row  lg:justify-center  lg:mx-0  lg:w-auto ">
+                        <button className="btn-download bg-[#26baa4] mb-6 lg:mb-0 lg:mr-4">Download for iOS</button>
+                        <button className="btn-download bg-[#6173ff]">Download for Mac</button>
         </div>
     </main>
   )
